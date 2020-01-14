3552228d332f26c1bf7722ecd843d69e49872914
David Huck <dnhuck@dmacc.edu>
2020-01-14 16:53:22 -0600

    added testGit.php

---
+++ testGit.php
@@ -0,0 +1,69 @@
+<?php
+	$firstName = 'David';
+	$lastName = 'Huck';
+	echo "My Name is $firstName $lastName (PHP)"
+;?>
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Day 1: Intro to PHP</title>
+</head>
+
+<body>
+	<h1>WDV341: Intro PHP</h1>
+	<h2>Assignment: Download Git Client & Create a Repository account</h2>
+	<p>Define the following Git related terms:</p>
+	<ul>
+		<li>Version Control Software</li>
+			<p>VCS is</p>
+		<li>Add</li>
+			<p>Add is adding the file to the staging area where it will be commited in the next step. This adds more security is you make a mistake by having an extra step to upload.</p>
+		<li>Commit</li>
+			<p>Commiting is like a snapshot of your code at the time of commit; therefore any changed made after the initial commit will have to be comminted again.</p>
+		<li>Push</li>
+			<p>After commiting, you need to push. Pushing is the actual moving of your files onto Github or your VCS.</p>
+		<li>Pull</li>
+			<p></p>
+		<li>Clone</li>
+			<p>Cloning is essentially copying work from a git repository and saving it locally.</p>
+	</ul>
+	
+</body>
+</html>
+
+
+
+
+
+
+<!-- <?php
+
+	//$firstName = 'David'; // Datatype:String Scope:Global
+	//$lastName = 'Huck'; // Datatype:String Scope:Global
+		
+?>
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Day 1: Intro to PHP</title>
+</head>
+
+<body>
+	<?php
+		//echo '<p>Hello, World</p>';
+	?>
+	<h2>Hello <?php //echo $firstName; ?></h2>
+	<h2>Hello <?php //echo $firstName . " " . //$lastName; ?></h2>
+	
+	<h1>WDV341: Intro PHP</h1>
+	
+	<?php 
+		//echo '<h3>PHP Wrote This Line</h3>';
+		//echo '<p> Paragraph written in php</p>';
+		//echo '<h2> Heading 2 written in php</h2>';
+	?>
+</body>
+</html> 
+-->
