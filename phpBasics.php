ac76a9f43a8bc93ca9b732244cdddc4023c80326
David Huck <dnhuck@dmacc.edu>
2020-01-18 19:02:34 -0600

    added File- PHP Basics

---
+++ phpBasics.php
@@ -0,0 +1,37 @@
+<?php
+
+	$yourName = 'David Huck';
+	$number1 = 10;
+	$number2 = 9;
+	$total = $number1 + $number2;
+	$languages = ['PHP', 'HTML', 'JavaScript'];
+?>
+<script>
+
+</script>
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>PHP Basics</title>
+</head>
+
+<body>
+	
+	<h1>PHP Basics</h1>
+	<h2><?php echo $yourName; ?></h2>
+	<h2><?php echo 'Displaying values below:'; ?></h2>
+	
+	<p><?php echo $number1; ?></p>
+	<p><?php echo $number2; ?></p>
+	<p><?php echo $total; ?></p>
+	
+	<h2>JS Array using PHP</h2>
+	<?php
+		foreach ($languages as $codes){
+		echo $codes . "<br>";
+	}
+	?>
+	
+</body>
+</html>
